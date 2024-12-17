4ab3ff70eafc3f9be71e0c47e06bfc85eb61b35e
testuser0123-web <s_aoki@ymail.ne.jp>
2024-12-18 07:58:27 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  build: {
+    outDir: "dist"
+  },
   css: {
     postcss: {
       plugins: [tailwindcss, autoprefixer],
